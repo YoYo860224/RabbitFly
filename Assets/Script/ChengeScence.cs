d0ec19a6cf05b4c7753c314eb4465106c8da376f
YoCheng <xyox860224@hotmail.com>
2017-10-22 18:44:05 +0800

    Remove dont need Add new pic

---
+++ Assets/Script/ChengeScence.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChengeScence : MonoBehaviour {
+    public int x = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void scenceChange()
+    {
+        SceneManager.LoadScene("Sce");
+    }
+}
