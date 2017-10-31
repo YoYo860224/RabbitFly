14dfb377a39e374be3835e9a23626d9058a44803
YoCheng <xyox860224@hotmail.com>
2017-10-31 10:55:56 +0800

    Fix animate Control

--- Assets/Script/Control.cs
+++ Assets/Script/Control.cs
@@ -3,30 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Control : MonoBehaviour {
+    //  get animator
     Animator anim;
+
+    //  checkGround
     [SerializeField] LayerMask whatIsGround;
-    Transform groundCheck;
     float groundedRadius = 1.0f;
     bool grounded = true;
 
+    //  Fint Heightest
+    Vector3 prePos;
+    Vector3 nowPos;
+
     // Use this for initialization
     void Start () {
-        //groundCheck = transform.Find("GroundCheck");
         anim = GetComponent<Animator>();
+        prePos = transform.position;
+        nowPos = transform.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
+
         grounded = Physics2D.OverlapCircle(transform.position, groundedRadius, whatIsGround);
         anim.SetBool("isGround", grounded);
         Debug.Log(grounded);
+
         if (Input.GetKeyDown(KeyCode.D)){
             //anim.SetBool("isGround", false);
             anim.SetTrigger("jumpR");
             GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
             GetComponent<Rigidbody2D>().AddForce(new Vector2(1000, 2000));
-            grounded = false;
-
+            //grounded = false;
         }
 
         if (Input.GetKeyDown(KeyCode.A))
@@ -35,17 +43,26 @@ public class Control : MonoBehaviour {
             anim.SetTrigger("jumpL");
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
             GetComponent<Rigidbody2D>().AddForce(new Vector2(-1000, 2000));
-            grounded = false;
+            //grounded = false;
         }
 
-
+        //  goDown
         if (Input.GetKeyDown(KeyCode.S))
         {
-            //anim.SetBool("isGround", true);
-            anim.SetTrigger("down");
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -10000));
+            if (!grounded)
+            {
+                anim.SetTrigger("down");
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -10000));
+            }
         }
 
+        prePos = nowPos;
+        nowPos = transform.position;
+
+        if (prePos.y > nowPos.y && !grounded)
+            anim.SetBool("Be_fallRound", true);
+        else
+            anim.SetBool("Be_fallRound", false);
     }
 }
