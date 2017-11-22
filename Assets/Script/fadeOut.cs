e6ba58bf55365add3c0945cb753b65adef00c38d
YoCheng <xyox860224@hotmail.com>
2017-11-23 01:42:26 +0800

    fix fadeout

--- Assets/Script/fadeOut.cs
+++ Assets/Script/fadeOut.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FadeOut : MonoBehaviour {
+public class fadeOut : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
