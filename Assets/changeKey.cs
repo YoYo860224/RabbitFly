a2945b335ef47ccd93f790806a6f279be7f968cf
YoCheng <xyox860224@hotmail.com>
2017-12-16 17:23:15 +0800

    add KeySet

---
+++ Assets/changeKey.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeKey : MonoBehaviour {
+
+    public string changeWhat;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (changeWhat != null)
+        {
+            foreach (KeyCode vKey in System.Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKey(vKey))
+                {
+                    PlayerPrefs.SetInt(changeWhat, (int)vKey);
+                    changeWhat = null;
+                    this.enabled = false;
+                }
+            }
+        }
+	}
+}
